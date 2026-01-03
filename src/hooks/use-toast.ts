c6428d2db914715c0db65a9e47250eaeea94686b
JamesAsuraA93 <AsuraA93@gmail.com>
2026-01-04 03:32:23 +0700

    feat: add toast component and integrate with use-toast hook
    - Added @radix-ui/react-toast dependency to package.json.
    - Removed unused resizable component from the codebase.
    - Implemented a new toast component with customizable variants and actions.
    - Updated use-toast hook to support the new toast component and its props.

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,18 +1,23 @@
 'use client'
 
+import { ToastActionElement } from '@/components/ui/toast'
 // Inspired by react-hot-toast library
 import * as React from 'react'
+import { ToasterProps } from 'sonner'
+// import { ToasterProps } from 'sonner'
 
-import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
+// import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
-type ToasterToast = ToastProps & {
+type ToasterToast = ToasterProps & {
   id: string
   title?: React.ReactNode
   description?: React.ReactNode
   action?: ToastActionElement
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
 }
 
 const actionTypes = {
@@ -155,7 +160,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
+      onOpenChange: (open: boolean) => {
         if (!open) dismiss()
       },
     },
